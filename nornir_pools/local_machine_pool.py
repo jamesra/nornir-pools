05f00d048a573896be42b9767900dfd06457eb94
James Anderson <james.r.andreson@utah.edu>
2015-03-10 12:37:04 -0700

    Do not create a pool only to call wait for completion

--- nornir_pools/local_machine_pool.py
+++ nornir_pools/local_machine_pool.py
@@ -14,11 +14,17 @@ class LocalMachinePool(poolbase.PoolBase):
 
     @property
     def _multithreading_pool(self):
-        return nornir_pools.GetMultithreadingPool(self.Name + " multithreading pool", self._num_threads)
+        if self._mtpool is None:
+            self._mtpool = nornir_pools.GetMultithreadingPool(self.Name + " multithreading pool", self._num_threads)
+        
+        return self._mtpool
 
     @property
     def _process_pool(self):
-        return nornir_pools.GetProcessPool(self.Name + " process pool", self._num_threads)
+        if self._ppool is None:
+            self._ppool = nornir_pools.GetProcessPool(self.Name + " process pool", self._num_threads)
+            
+        return self._ppool
 
     def get_active_nodes(self):
         return ["localhost"]
@@ -29,10 +35,10 @@ class LocalMachinePool(poolbase.PoolBase):
         '''
 
         self._num_threads = num_threads
-        # self._name = pool_name
-
-        self._multithreadpool = None
-        self._processpool = None
+        
+        self._mtpool = None
+        self._ppool = None
+ 
 
     def add_task(self, name, func, *args, **kwargs):
         return self._multithreading_pool.add_task(name, func, *args, **kwargs)
@@ -43,8 +49,11 @@ class LocalMachinePool(poolbase.PoolBase):
     def wait_completion(self):
 
         """Wait for completion of all the tasks in the queue"""
-        self._multithreading_pool.wait_completion()
-        self._process_pool.wait_completion()
-
+        if not self._mtpool is None:
+            self._multithreading_pool.wait_completion()
+            
+        if not self._ppool is None:
+            self._process_pool.wait_completion()
+        
     def shutdown(self):
         self.wait_completion()
